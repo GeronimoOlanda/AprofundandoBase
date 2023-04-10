77693e19cca6193978eb90b6617239cb01cc7ff1
GeronimoOlanda <geronimo.olanda@outlook.com>
2023-04-10 00:18:15 -0300

    up

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,22 @@ let ano = document.querySelector(".ano");
 let getData = new Date();
 
 ano.innerHTML = getData.getFullYear();
+
+const buttons = document.querySelectorAll('.nav-item-link');
+
+
+
+
+// adicionando cor fixa apos o click //
+// Adicione um evento de clique a cada botão
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Remova a classe "selected" de todos os botões
+    buttons.forEach(btn => {
+      btn.classList.remove('selected');
+    });
+
+    // Adicione a classe "selected" ao botão clicado
+    button.classList.add('selected');
+  });
+});
