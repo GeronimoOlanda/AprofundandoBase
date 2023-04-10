88c77fed05dabfaf9c445a628da0ef9aa8dffe20
GeronimoOlanda <geronimo.olanda@outlook.com>
2023-04-10 01:04:22 -0300

    formatação

--- src/js/index.js
+++ src/js/index.js
@@ -11,6 +11,7 @@ const buttons = document.querySelectorAll('.nav-item-link');
 // adicionando cor fixa apos o click //
 // Adicione um evento de clique a cada botão
 buttons.forEach(button => {
+    debugger
   button.addEventListener('click', () => {
     // Remova a classe "selected" de todos os botões
     buttons.forEach(btn => {
