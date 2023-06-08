8c1cd66aef0d0297833399b299202212cefe9d3c
GeronimoOlanda <geronimo.olanda@outlook.com>
2023-06-08 20:55:06 -0300

    teste

--- GethaWebSite/GethaWebSite/GethaWebSite/Models/PaginaInicial/PaginaInicialModel.cs
+++ GethaWebSite/GethaWebSite/GethaWebSite/Models/PaginaInicial/PaginaInicialModel.cs
@@ -1,6 +1,11 @@
-﻿namespace GethaWebSite.Models.PaginaInicial
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GethaWebSite.Models.PaginaInicial
 {
     public class PaginaInicialModel
     {
+        public string id { get; set; }  
     }
 }
